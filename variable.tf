fd77d306f2b509e493e64417f09a246ecd509416
Boogeyman97-ops <67091478+Boogeyman97-ops@users.noreply.github.com>
2020-08-23 17:54:39 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,30 +1,7 @@
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+  subscription_id = "abb119be-c8dd-42be-9b60-64269a565c61"
+  client_id       = "087ab2b7-78f7-42a1-aa68-625d9f2ab574"
+  client_secret   = "vo~Nfe-uQ.z4YGkFT6iYn-G5oE9~ko30se"
+  tenant_id       = "426008e7-7e3f-4ad1-ba2d-7fd1e5c4d3b2"
   features {}
-}
-
-variable "subscription_id" {
-  description = "Enter Subscription ID for provisioning resources in Azure"
-}
-
-variable "client_id" {
-  description = "Enter Client ID for Application created in Azure AD"
-}
-
-variable "client_secret" {
-  description = "Enter Client secret for Application in Azure AD"
-}
-
-variable "tenant_id" {
-  description = "Enter Tenant ID / Directory ID of your Azure AD. Run Get-AzureSubscription to know your Tenant ID"
-}
-{
-  "appId": "087ab2b7-78f7-42a1-aa68-625d9f2ab574",
-  "displayName": "Terraform-sp-1",
-  "name": "http://Terraform-sp-1",
-  "password": "vo~Nfe-uQ.z4YGkFT6iYn-G5oE9~ko30se",
-  "tenant": "426008e7-7e3f-4ad1-ba2d-7fd1e5c4d3b2"
 }
