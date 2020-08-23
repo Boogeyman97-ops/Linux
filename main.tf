2456836378be4dc802b0db47a558df6ba0500066
Boogeyman97-ops <67091478+Boogeyman97-ops@users.noreply.github.com>
2020-08-23 16:04:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,41 +2,41 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "Terraform_rgroup" {
+resource "azurerm_resource_group" "terraform_rgroup" {
   name     = "bm-resources"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "Terraform_vnet" {
+resource "azurerm_virtual_network" "terraform_vnet" {
   name                = "bm-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.Terraform_rgroup.location
-  resource_group_name = azurerm_resource_group.Terraform_rgroup.name
+  location            = azurerm_resource_group.terraform_rgroup.location
+  resource_group_name = azurerm_resource_group.terraform_rgroup.name
 }
 
-resource "azurerm_subnet" "Terraform_subnet" {
+resource "azurerm_subnet" "terraform_subnet" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.Terraform_rgroup.name
-  virtual_network_name = azurerm_virtual_network.Terraform_vnet.name
+  resource_group_name  = azurerm_resource_group.terraform_rgroup.name
+  virtual_network_name = azurerm_virtual_network.terraform_vnet.name
   address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_network_interface" "Terraform_interface" {
+resource "azurerm_network_interface" "terraform_interface" {
   name                = "bm-nic"
-  location            = azurerm_resource_group.Terraform_rgroup.location
-  resource_group_name = azurerm_resource_group.Terraform_rgroup.name
+  location            = azurerm_resource_group.terraform_rgroup.location
+  resource_group_name = azurerm_resource_group.terraform_rgroup.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.Terraform_subnet.id
+    subnet_id                     = azurerm_subnet.terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "Linux-1" {
-  name                = "Linux-machine"
-  resource_group_name = azurerm_resource_group.Terraform_rgroup.name
-  location            = azurerm_resource_group.Terraform_rgroup.location
+resource "azurerm_linux_virtual_machine" "linux-1" {
+  name                = "linux_machine"
+  resource_group_name = azurerm_resource_group.terraform_rgroup.name
+  location            = azurerm_resource_group.terraform_rgroup.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
